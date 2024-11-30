34daa1d4ce027646676dfa918900ff4e8e42d7c1
Qriosity12 <2987991635@qq.com>
2024-12-01 00:23:58 +0800

    Farm try1（初始化一块黑屏）

--- StardewValley/proj.win32/FarmScene.cpp
+++ StardewValley/proj.win32/FarmScene.cpp
@@ -159,3 +159,4 @@ void FarmScene::checkForElementInteraction(const cocos2d::Vec2& clickPos) {
         }
     }
 }
+
--- StardewValley/proj.win32/InitialScene.cpp
+++ StardewValley/proj.win32/InitialScene.cpp
@@ -2,6 +2,7 @@
 #include "cocos2d.h"
 #include "ui/CocosGUI.h"
 #include"FarmScene.h"
+#include"SoilScene.h"
 using namespace cocos2d;
 
 InitialScene::InitialScene() {
@@ -48,7 +49,7 @@ bool InitialScene::init()
     titleLabel->setPosition(Vec2(screenSize.width / 2, screenSize.height * 0.8f));
     this->addChild(titleLabel);
 
-    //以下全为gyx为了测试地图以及游玩而写
+    //以下为gyx为了测试地图以及游玩而写
      //创建按钮,确定键
     auto startButton = cocos2d::ui::Button::create("photo/startup_p/button1.png", "button_pressed.png", "button_disabled.png");
     // 设置按钮位置
@@ -63,6 +64,22 @@ bool InitialScene::init()
         cocos2d::Director::getInstance()->replaceScene(transition);
         });
     this->addChild(startButton);
+
+
+    //以下为zsk为了测试种地(但是初始化一直是一块黑屏我请问呢）
+    //创建按钮,确定键(左边那个)
+    auto startButton2 = cocos2d::ui::Button::create("photo/startup_p/button1.png", "button_pressed.png", "button_disabled.png");
+    // 设置按钮位置
+    startButton2->setPosition(Vec2(screenSize.width /4, screenSize.height / 4));
+    // 设置按钮大小，确保按钮不会超出屏幕
+    startButton2->setScale(0.5f);  // 可根据需要调整按钮大小
+
+    // 设置按钮点击事件，连接到农场土地
+    startButton2->addClickEventListener([=](Ref* sender) {
+        // 切换到游戏场景
+        Director::getInstance()->replaceScene(new SoilScene());
+        });
+    this->addChild(startButton2);
 
     return true;
 }
