34352bd4151ca215c21c8329e90806df793858aa
Qriosity12 <2987991635@qq.com>
2024-11-30 15:43:20 +0800

    Farm changes

--- StardewValley/Classes/Crop.h
+++ StardewValley/Classes/Crop.h
@@ -1,20 +1,23 @@
 #ifndef __CROP_H__
 #define __CROP_H__
 
+#include <vector>
 #include <string>
+#include "cocos2d.h"
 
 class Crop {
 public:
-    Crop(const std::string& name, int growthTime);
-    void grow();
-    bool isMature() const;
-    std::string getName() const;
+    Crop(const std::string& name, const std::vector<std::string>& stages);
+    void grow(); // 作物生长
+    bool isMature() const; // 作物是否成熟
+    cocos2d::Sprite* getSprite() const; // 获取作物对应的Sprite
+    void setPosition(cocos2d::Vec2 position); // 设置作物位置
 
 private:
     std::string name;
-    int growthTime;   // 成长所需时间
-    int currentAge;   // 当前生长阶段
+    int currentAge; // 当前生长阶段
+    std::vector<std::string> stages; // 不同阶段对应的图片路径
+    cocos2d::Sprite* sprite; // 作物的Sprite
 };
 
 #endif
-#pragma once
--- StardewValley/Classes/FarmScene.h
+++ StardewValley/Classes/FarmScene.h
@@ -2,8 +2,8 @@
 #define __FARM_SCENE_H__
 
 #include "cocos2d.h"
-#include "Crop.h"
 #include "Tile.h"
+#include "Crop.h"
 #include <vector>
 
 class FarmScene : public cocos2d::Scene {
@@ -12,18 +12,15 @@ public:
     FarmScene();
     ~FarmScene();
 
-    void update(float dt);
-    void plantCrop(int x, int y, Crop* crop);
-    void waterCrop(int x, int y);
-    void harvestCrop(int x, int y);
-    void renderFarm(cocos2d::Layer* layer);
+    void update(float dt); // 每帧更新
+    void renderFarm(cocos2d::Layer* layer); // 渲染农场
+    void plantCrop(int x, int y, Crop* crop); // 在指定位置种植作物
+    void waterCrop(int x, int y); // 浇水
 
 private:
-    std::vector<std::vector<Tile>> farmGrid;
     int farmWidth;
     int farmHeight;
+    std::vector<std::vector<Tile>> farmGrid; // 存储农场的格子
 };
 
 #endif
-
-#pragma once
