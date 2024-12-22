7143f414efd8481a68aa0b6d5a421cc75aec2cf7
liyexiaoxiao <2945107252@qq.com>
2024-12-22 17:53:43 +0800

    refresh taskbar

--- StardewValley/proj.win32/TaskBar.cpp
+++ StardewValley/proj.win32/TaskBar.cpp
@@ -20,16 +20,16 @@ bool TaskBar::init() {
     // 初始化任务列表
     taskList.clear();
 
-    addTask("Alex","photo/task/alex.png", "photo/storage/beer.png",15,1);
-    addTask("Willy", "photo/task/willy.png", "photo/storage/Fish_1.png", 5,2);
-    addTask("Marnie", "photo/task/marnie.png", "photo/storage/hongmogu.png", 20, 3);
-    addTask("Sandy", "photo/task/sandy.png", "photo/storage/jiang.png", 25, 4);
-    addTask("Clint", "photo/task/clint.png", "photo/storage/luobo.png", 8, 5);
-    addTask("Willy", "photo/task/willy.png", "photo/storage/mutou.png", 50, 6);
-    addTask("Sandy", "photo/task/sandy.png", "photo/storage/cooked_fish.png", 2, 7);
-    addTask("Marnie", "photo/task/marnie.png", "photo/storage/dacong.png", 15, 8);
-    addTask("Clint", "photo/task/clint.png", "photo/storage/nangua.png", 20, 9);
-    addTask("Alex", "photo/task/alex.png", "photo/storage/shitou.png", 50, 10);
+    addTask("Alex","photo/task/alex.png", "photo/storage/beer.png",150,1);
+    addTask("Willy", "photo/task/willy.png", "photo/storage/Fish_1.png", 50,2);
+    addTask("Marnie", "photo/task/marnie.png", "photo/storage/hongmogu.png", 200, 3);
+    addTask("Sandy", "photo/task/sandy.png", "photo/storage/jiang.png", 250, 4);
+    addTask("Clint", "photo/task/clint.png", "photo/storage/luobo.png", 80, 5);
+    addTask("Willy", "photo/task/willy.png", "photo/storage/mutou.png", 500, 6);
+    addTask("Sandy", "photo/task/sandy.png", "photo/storage/cooked_fish.png", 20, 7);
+    addTask("Marnie", "photo/task/marnie.png", "photo/storage/dacong.png", 150, 8);
+    addTask("Clint", "photo/task/clint.png", "photo/storage/nangua.png", 200, 9);
+    addTask("Alex", "photo/task/alex.png", "photo/storage/shitou.png", 500, 10);
 
     return true;
 }
@@ -64,7 +64,7 @@ void TaskBar::addTask(const std::string& title, const std::string& RenPhoto, con
 
     // 计算按钮的位置
     float posX = visibleSize.width / 2-100;
-    if (taskNum > 5) {0
+    if (taskNum > 5) {
         posX += 350;
     }
     float posY = visibleSize.width / 2 - 400;
@@ -77,11 +77,11 @@ void TaskBar::addTask(const std::string& title, const std::string& RenPhoto, con
     float Scale = 0.8f;
     if (title == "Sandy" || title == "Clint")
         Scale = 3.0f;
-    float RenPosX=posX-200;
+    float RenPosX=posX-210;
     this->addChild(Tool.addImageToScene(RenPhoto, cocos2d::Vec2(RenPosX, posY), Scale),20);
     float CailiaoPosX = RenPosX + 50;
     this->addChild(Tool.addImageToScene(CailiaoPhoto, cocos2d::Vec2(CailiaoPosX, posY), 2.8f),20);
-    float CailiaoLabelPosX = CailiaoPosX + 30;
+    float CailiaoLabelPosX = CailiaoPosX + 35;
     this->addChild(Tool.createLabel(std::to_string(CailiaoNum), "fonts/Marker Felt.ttf", 24, cocos2d::Vec2(CailiaoLabelPosX, posY)),20);
     // 创建任务结构并添加到任务列表
     Task task;
